45819665591afa8e6fd59fc34beda382dbe65f4d
Patricia <pbravo.art@gmail.com>
2025-06-16 17:10:35 -0400

    Update des styles Tailwind sur SectionIntro

--- travel-frontend/src/components/SectionIntro.vue
+++ travel-frontend/src/components/SectionIntro.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="bg-green-50 dark:bg-gray-900">
+  <section class="bg-green-100 dark:bg-gray-900">
     <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
       <div class="font-light text-gray-700 sm:text-lg dark:text-gray-400">
         <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-green-800 dark:text-white">Bienvenue en Colombie, un véritable paradis sur Terre</h2>
@@ -8,8 +8,7 @@
 Que tu sois amateur de randonnée, de plages ou de vie urbaine, la Colombie t’accueille les bras ouverts pour des vacances inoubliables.</p>
       </div>
       <div class="grid grid-cols-2 gap-4 mt-8">
-        <img class="w-full rounded-lg" :src="destination1" alt="Parc Tayrona, Colombie">
-        <img class="w-full rounded-lg" :src="destination2" alt="Le rocher de Guatapé, Colombie">
+        <img class="w-full rounded-lg col-span-2" :src="destination1" alt="Parc Tayrona, Colombie">
         <img class="w-full rounded-lg" :src="destination3" alt="Parc Tayrona, Colombie">
         <img class="w-full rounded-lg" :src="destination4" alt="Parc Tayrona, Colombie">
       </div>
@@ -19,7 +18,6 @@ Que tu sois amateur de randonnée, de plages ou de vie urbaine, la Colombie t’
 
 <script>
 import destination1 from '@/assets/images/tayrona.jpg'
-import destination2 from '@/assets/images/guatape.jpg'
 import destination3 from '@/assets/images/tayrona2.jpg'
 import destination4 from '@/assets/images/tayrona3.jpg'
 
@@ -28,7 +26,6 @@ export default {
   data () {
     return {
       destination1,
-      destination2,
       destination3,
       destination4
     }
